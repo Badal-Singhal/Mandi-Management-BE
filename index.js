6ca1413d147fd287f43f844dceff33995ecd653b
Badal-Singhal <146357764+Badal-Singhal@users.noreply.github.com>
2024-09-03 16:48:32 +0530

    "env added"

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = require("./API/routes/AppRoutes");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 var cors = require("cors");
 
 app.use(cors());
