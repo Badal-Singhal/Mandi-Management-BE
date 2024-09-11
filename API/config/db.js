c3cedc023cc6838d1f24d6d4fdbb5030d8020158
Badal-Singhal <146357764+Badal-Singhal@users.noreply.github.com>
2024-09-11 19:40:27 +0530

    changes as perf production

--- API/config/db.js
+++ API/config/db.js
@@ -1,12 +1,20 @@
 const mysql=require('mysql');
 
-
+//local
 const conn=mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'Badal@123',
     database:'mandi'
 })
+
+//production
+// const conn=mysql.createConnection({
+//     host:'127.0.0.1',
+//     user:'root',
+//     password:'Aaditya@123',
+//     database:'mandi'
+// })
 
 conn.connect((err)=>{
     if(err) throw err
--- index.js
+++ index.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const router = require("./API/routes/AppRoutes");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 var cors = require("cors");
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/mandi", router);
+
 
 app.listen(port, () => {
   console.log("server is listening on port " + port);
